89f038cf246febad19532889c7286c6999f3c1e6
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2021-10-18 02:19:08 -0300

    Create job_case3.sh

---
+++ job_case3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-aschmidt  # replace this with your supervisors account
+#SBATCH --nodes=1                # number of node MUST be 1
+#SBATCH --cpus-per-task=4        # number of processes
+#SBATCH --mem-per-cpu=8192M      # memory; default unit is megabytes
+#SBATCH --time=3-00:00           # time (DD-HH:MM)
+#SBATCH --mail-user=widemberg@dme.ufrj.br # Send email updates to you or someone else
+#SBATCH --mail-type=ALL          # send an email in all cases (job started, job ended, job aborted)
+
+module load gcc/9.3.0 r/4.1.0
+
+# Export the nodes names. 
+# If all processes are allocated on the same node, NODESLIST contains : node1 node1 node1 node1
+# Cut the domain name and keep only the node name
+export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
+R -f TBsetting_case3.R
