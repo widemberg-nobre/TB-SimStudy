ee9855de95098a8149890d5573665fbbd3e07112
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2021-10-18 16:51:39 -0300

    Update job_out_case3.sh

--- job_out_case3.sh
+++ job_out_case3.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=widemberg@dme.ufrj.br # Send email updates to you or someone else
 #SBATCH --mail-type=ALL          # send an email in all cases (job started, job ended, job aborted)
 
-module load gcc/9.3.0 r/4.1.0
+ml gcc r-bundle-bioconductor
 
 # Export the nodes names. 
 # If all processes are allocated on the same node, NODESLIST contains : node1 node1 node1 node1
