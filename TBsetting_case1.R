2f892a4a69c8ab3ac33709d68e7da1f15230a9c3
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2021-10-18 04:00:48 -0300

    Update TBsetting_case1.R

--- TBsetting_case1.R
+++ TBsetting_case1.R
@@ -1,3 +1,6 @@
+Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1) # only necessary for Linux without the nodejs library / headers
+install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+
 require(rstan)
 require(mnormt)
 options(mc.cores = parallel::detectCores())
