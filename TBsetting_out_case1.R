092233c04f4143996fd43cb5bf7f68a504800420
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2021-10-20 16:03:50 -0300

    Update TBsetting_out_case1.R

--- TBsetting_out_case1.R
+++ TBsetting_out_case1.R
@@ -44,10 +44,10 @@ for(k in 1){
     
     max.Rhat.Yre1[k,w] <- max(stan_rhat(fitOut3)$`data`)
     max.Rhat.Yre2[k,w] <- max(stan_rhat(fitOut4)$`data`)
-    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_case1out.RData")}
+    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_case1out_ScenX2.RData")}
   }
-  save.image("/project/6003552/widloro/git/TBsim_binbin_case1out.RData")
-  object4 <- stan_model(stan_model("/project/6003552/widloro/git/outcome_bernoulli_model.stan"))
+  save.image("/project/6003552/widloro/git/TBsim_binbin_case1out_ScenX2.RData")
+  object4 <- stan_model("/project/6003552/widloro/git/outcome_bernoulli_model.stan")
   for(w in 1:nsim){
     data.3 <- list(N = m*nrep,M=m,I=index, 
                    Z = data[[k]][[w]]$Z, Y = data[[k]][[w]]$Y, 
@@ -73,7 +73,7 @@ for(k in 1){
     OddsRatio.ests[[k]][w,1] <- mean(exp(chain.beta1$beta))
     OddsRatio.ests[[k]][w,2] <- mean(exp(chain.beta2$beta))
     
-    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_case1out.RData")}
+    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_case1out_ScenX2.RData")}
   }
 }
-save.image("/project/6003552/widloro/git/TBsim_binbin_case1out.RData")
+save.image("/project/6003552/widloro/git/TBsim_binbin_case1out_ScenX2.RData")
