00aa65d2eec868b8294c5f10665ceeb3982ac664
widemberg-nobre <75651911+widemberg-nobre@users.noreply.github.com>
2021-10-23 18:08:26 -0300

    Update and rename TBsetting_out_case2-2.R to TBsetting_out_Scen2_gera_X_1.R

--- TBsetting_out_Scen2_gera_X_1.R
+++ TBsetting_out_Scen2_gera_X_1.R
@@ -9,15 +9,14 @@ rstan_options(auto_write = TRUE)
 
 beta.ests <-  var.beta.ests  <- OddsRatio.ests <- replicate(3,data.frame())
 
+object4 <- stan_model("/project/6003552/widloro/git/outcome_bernoulli_model.stan")
 
-load("/project/6003552/widloro/git/TBsim_binbin_case2out_ScenX2.RData")
-
-max.Rhat.Yre1 <- max.Rhat.Yre2 <- matrix(NA,3,nsim)
-
-for(k in 2){
+for(k in 1:3){
+  if(k==1){("/project/6003552/widloro/git/TBsim_binbin_case1Exp_Scen2_de_X.RData")}
+  if(k==2){("/project/6003552/widloro/git/TBsim_binbin_case2Exp_Scen2_de_X.RData")}
+  if(k==3){("/project/6003552/widloro/git/TBsim_binbin_case3Exp_Scen2_de_X.RData")}
   beta.ests[[k]] <- OddsRatio.ests[[k]] <- var.beta.ests[[k]] <-  matrix(NA,nsim,4)
-  object4 <- stan_model("/project/6003552/widloro/git/outcome_bernoulli_model.stan")
-  for(w in 951:nsim){
+  for(w in 1:nsim){
     data.3 <- list(N = m*nrep,M=m,I=index, 
                    Z = data[[k]][[w]]$Z, Y = data[[k]][[w]]$Y, 
                    X=cbind(1,prop.score.1[[w]]),
@@ -41,7 +40,7 @@ for(k in 2){
     chain.beta2 <- extract(fitOut2,'beta')
     OddsRatio.ests[[k]][w,1] <- mean(exp(chain.beta1$beta))
     OddsRatio.ests[[k]][w,2] <- mean(exp(chain.beta2$beta))
-    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_case2out_ScenX2.RData")}
+    if(w %in% seq(50,nsim,len=20)){print(w);print(timestamp());save.image("/project/6003552/widloro/git/TBsim_binbin_Scen2_de_X_MD1_e_MD2.RData")}
   }
 }
-save.image("/project/6003552/widloro/git/TBsim_binbin_case2out_ScenX2.RData")
+save.image("/project/6003552/widloro/git/TBsim_binbin_Scen2_de_X_MD1_e_MD2.RData")
